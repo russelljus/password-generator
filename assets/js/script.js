17510fc6198400c914e0261c09d1866d668bb0fa
Justin Russell <justinrussell@Justins-MacBook-Pro.local>
2021-04-01 11:14:42 -0600

    user-input

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,30 @@ var lowerCaseArray = [  'a',  'b',  'c',  'd',  'e',  "f",  'g',  'h',  'i',  'j
 var upperCaseArray = [  'A',  'B',  'C',  'D',  'E',  "F",  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N', 'O',  'P',  'Q',  'R',  'S',  'R',  'U',  'V',  'W',  'X',  'Y',  'Z'];
 
 
+function userInput () {
+    // prompts command for user input
+    var charaterLength = prompt ("how long do you want your password to be?")
+    console.log (charaterLength)
+    // Confirms return a boolean value
+    var upperCase = confirm ("do you want uppercase letters in your password?")
+    var lowerCase = confirm ("do you want lowercase characters in your password?")
+    var numbers = confirm ("do you want numbers in your password?")
+    var special = confirm ("do you want special characters in your password?")
+
+
+}
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    var password = userInput();
     var passwordText = document.querySelector("#password");
   
     passwordText.value = password;
