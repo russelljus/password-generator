63d40410cc1432fd9fc4f707fb3fa14bfa3d669d
Justin Russell <justinrussell@Justins-MacBook-Pro.local>
2021-04-01 10:03:58 -0600

    initial code setup

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+// Assignment Code
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
